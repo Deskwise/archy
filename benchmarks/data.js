7a36a20c306c9748e82dff4c656a77722e3d65e9
github-action-benchmark <github@users.noreply.github.com>
2025-08-06 17:45:56 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for e76b85cfec24e60ced4d9974fff05916778a1a15

---
+++ benchmarks/data.js
@@ -0,0 +1,60 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1754502356429,
+  "repoUrl": "https://github.com/Deskwise/archy",
+  "entries": {
+    "n8n-mcp Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "richardruiz@live.com",
+            "name": "Richard Ruiz",
+            "username": "richrz"
+          },
+          "committer": {
+            "email": "richardruiz@live.com",
+            "name": "Richard Ruiz",
+            "username": "richrz"
+          },
+          "distinct": true,
+          "id": "e76b85cfec24e60ced4d9974fff05916778a1a15",
+          "message": "Initial commit to Archy repo",
+          "timestamp": "2025-08-06T11:44:25-06:00",
+          "tree_id": "afbae5680a4c00d5cb405f41699bfe9e07b7aa89",
+          "url": "https://github.com/Deskwise/archy/commit/e76b85cfec24e60ced4d9974fff05916778a1a15"
+        },
+        "date": 1754502356040,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0195,
+            "unit": "ms",
+            "range": 0.2718,
+            "extra": "51283 ops/sec"
+          },
+          {
+            "name": "sample - array sorting - large",
+            "value": 3.1682,
+            "unit": "ms",
+            "range": 0.6984999999999997,
+            "extra": "316 ops/sec"
+          },
+          {
+            "name": "sample - string concatenation",
+            "value": 0.0047,
+            "unit": "ms",
+            "range": 0.2801,
+            "extra": "212513 ops/sec"
+          },
+          {
+            "name": "sample - object creation",
+            "value": 0.066,
+            "unit": "ms",
+            "range": 0.3272,
+            "extra": "15145 ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
